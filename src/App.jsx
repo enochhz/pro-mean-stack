98787c5b5212943aa3b2f54edc44dbd46be76091
walkerzheng <walkerhaozheng@gmail.com>
2017-10-21 10:26:36 -0700

    add concurrently

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 var contentNode = document.getElementById('contents');
 
 const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+const message = continents.map(c => `Hello ${c}!!`).join(' ');
 var component = <p>{message}</p>;
 ReactDOM.render(component, contentNode);
