89399783e49add95a405b6cb2a0f83ea8cfc2a8a
walkerzheng <9771330@qq.com>
2017-10-22 13:00:40 -0700

    passing data

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,43 @@ class IssueFilter extends React.Component {
   }
 }
 
+class IssueRow extends React.Component {
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 4};
+    return(
+      <tr>
+        <td style={borderedStyle}>{this.props.issue_id}</td>
+        <td style={borderedStyle}>{this.props.children}</td>
+      </tr>
+    );
+  }
+}
+
+IssueRow.propTypes = {
+  issue_id : React.PropTypes.number.isRequired,
+  issue_title: React.PropTypes.string
+};
+
+IssueRow.defaultProps = {
+  issue_title: '-- default title --',
+};
+
 class IssueTable extends React.Component {
   render() {
+    const borderedStyle = {border: "1px solid silver", padding: 6};
     return (
-      <div>This is a placeholder for a table of Issues.</div>
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={borderedStyle}>Id</th>
+            <th style={borderedStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow issue_id={1}>Error in console when clicking Add</IssueRow>
+          <IssueRow issue_id={2}>Missing bottom <b>border</b> on panel</IssueRow>
+        </tbody>
+      </table>
     );
   }
 }
